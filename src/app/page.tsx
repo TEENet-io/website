b4678662e34b859b859233c806fd64246da4e3c1
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2025-09-04 21:25:36 +0800

    font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,10 +67,10 @@ export default function Home() {
         <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8 text-center">
             <div className="mx-auto max-w-4xl">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              <h1 className="circle-h1 font-bold tracking-tight text-gray-900">
                 Trustless Off-Chain Execution with Built‑In Private Key Security
               </h1>
-              <p className="mt-4 text-xl text-gray-500">
+              <p className="mt-4 circle-large-text text-gray-500">
                 A trustless TEE-powered off-chain execution layer with built-in key management, purpose-built for decentralized applications.
               </p>
             </div>
